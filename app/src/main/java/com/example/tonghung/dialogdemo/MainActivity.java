e49789e3e44fd9cfeea9dd3b83a1c268ecf5c42d
tonghung <gcbtfh@gmail.com>
2016-12-10 10:03:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tonghung/dialogdemo/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.tonghung.dialogdemo;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("This is Title");
+                dialog.setMessage("This is Message");
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "You clicked YES option!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialog.setNegativeButton("No", null);
+
+                dialog.show();
+            }
+        });
+    }
+}
